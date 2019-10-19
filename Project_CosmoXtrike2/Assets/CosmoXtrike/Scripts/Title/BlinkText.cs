0195b268ff12fb68bbbbeeab05bdaf717c6ddb28
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-10-19 20:10:16 +0900

    プロジェクトの新規作成と、タイトル部分の追加、ボイスデータの追加

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Title/BlinkText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkText : MonoBehaviour
+{
+    private float _nextTime;
+    private readonly float _interval = 0.6f;
+    
+    void Start () {
+        _nextTime = Time.time;
+    }
+
+    
+    void Update () {
+        if (Time.time > _nextTime) {
+            float alpha = gameObject.GetComponent<CanvasRenderer>().GetAlpha();
+            if (alpha == 1.0f) {
+                gameObject.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
+            } else {
+                gameObject.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+            }
+            _nextTime += _interval;
+        }
+    }
+}
