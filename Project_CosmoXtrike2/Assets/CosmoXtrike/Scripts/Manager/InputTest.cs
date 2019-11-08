246f2e79aeb8281f4cedbd2dcd6d7096cff86769
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-11-08 20:15:12 +0900

    傾き、移動の追加

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Manager/InputTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTest : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text inputText, inputText2;
+
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        DownKeyCheck();
+    }
+
+
+    void DownKeyCheck()
+    {
+        inputText.text = "R_Horizontal=" + Input.GetAxis("Right_Horizontal").ToString("F2") + " : R_Vertical" + Input.GetAxis("Right_Vertical").ToString("F2");
+        inputText2.text = "L_Horizontal=" + Input.GetAxis("Left_Horizontal").ToString("F2") + " : L_Vertical" + Input.GetAxis("Left_Vertical").ToString("F2");
+        
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire1");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("Fire2");
+        }
+
+        //if (Input.anyKeyDown)
+        //{
+        //    foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+        //    {
+        //        if (Input.GetKeyDown(code))
+        //        {
+        //            //処理を書く
+        //            Debug.Log(code);
+        //            break;
+        //        }
+        //    }
+        //}
+    }
+
+
+}
--- Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Manager/ShipController.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Manager/ShipController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipController : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class ShipController : MonoBehaviour
     [SerializeField]
     private GameObject ship;
     [SerializeField, Header("移動速度")]
-    private Vector2 movePower = new Vector2 (1,1);
+    private Vector2 movePower = new Vector2(1, 1);
     [SerializeField, Header("速度限界")]
     private Vector2 maxSpeed = new Vector2(100, 50);
     [SerializeField, Header("移動限界座標")]
@@ -21,14 +22,83 @@ public class ShipController : MonoBehaviour
     private Vector2 angle = new Vector2(11, 20);
     #endregion
 
+    Rigidbody rig;
+
+    [SerializeField]
+    private Text velocityText;
 
     void Start()
     {
-        
+        rig = ship.GetComponent<Rigidbody>();
     }
     
     void Update()
     {
 
+    }
+
+    void FixedUpdate()
+    {
+        Moving();
+        Rotating();
+    }
+
+    void Moving()
+    {
+        //  移動処理
+        float moveX = Input.GetAxisRaw("Left_Horizontal") * Time.deltaTime * movePower.x;
+        float moveY = Input.GetAxisRaw("Left_Vertical") * Time.deltaTime * movePower.y;
+        if (moveX != 0 || moveY != 0)
+        {
+            rig.AddForce(moveX, moveY, 0);
+            if (Mathf.Abs(rig.velocity.x) > maxSpeed.x)
+            {
+                if (rig.velocity.x > 0)
+                {
+                    rig.velocity = new Vector3(Mathf.Min(rig.velocity.x, maxSpeed.x), rig.velocity.y, 0);
+                }
+                if (rig.velocity.x < 0)
+                {
+                    rig.velocity = new Vector3(Mathf.Max(rig.velocity.x, maxSpeed.x), rig.velocity.y, 0);
+                }
+            }
+            if (Mathf.Abs(rig.velocity.y) > maxSpeed.y)
+            {
+                if (rig.velocity.y > 0)
+                {
+                    rig.velocity = new Vector3(rig.velocity.x, Mathf.Min(rig.velocity.y, maxSpeed.y), 0);
+                }
+                if (rig.velocity.y < 0)
+                {
+                    rig.velocity = new Vector3(rig.velocity.x, Mathf.Max(rig.velocity.y, maxSpeed.y), 0);
+                }
+            }
+        }
+        velocityText.text = rig.velocity.ToString("F2");
+
+        //  ブレーキ処理
+        if (moveX == 0 && moveY == 0)
+        {
+            //  一定値未満で切り捨て
+            if (rig.velocity.x < 0.1f && rig.velocity.x > -0.1f)
+                rig.velocity = new Vector2(0, rig.velocity.y);
+            if (rig.velocity.y < 0.1f && rig.velocity.y > -0.1f)
+                rig.velocity = new Vector2(rig.velocity.x, 0);
+            //  減速
+            rig.velocity = new Vector3(rig.velocity.x * brakePower.x, rig.velocity.y * brakePower.y);
+        }
+
+    }
+
+    private float rotateTime = 0;
+
+    private void Rotating()
+    {
+        float rotateX = Input.GetAxis("Right_Horizontal");
+        float rotateY = Input.GetAxis("Right_Vertical");
+        //if (rotateX <= )
+        Debug.Log("RotateX = " + rotateX + " : RotateY = " + rotateY);
+        //rig.rotation = Quaternion.Euler(Mathf.Lerp(rotateY, 0, Time.deltaTime), 0, Mathf.Lerp(rotateX, 0, Time.deltaTime));
+        rig.rotation = Quaternion.Euler(rotateY * -angle.y, 0, rotateX * -angle.x);
     }
 }
