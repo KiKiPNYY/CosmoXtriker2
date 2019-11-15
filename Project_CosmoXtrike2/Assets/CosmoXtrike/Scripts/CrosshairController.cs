aef9c64b8eef0fc1660789c4fd6f2117f6cbc081
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-11-15 19:19:34 +0900

    VRのプラグインと傾きの調整

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/CrosshairController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CrosshairController : MonoBehaviour
+{
+    //最大射角
+    [SerializeField]
+    float MaxDegreeX;
+    [SerializeField]
+    float MinDegreeX;
+    [SerializeField]
+    float MaxDegreeY;
+    [SerializeField]
+    float MinDegreeY;
+
+
+    private Vector3 _angle = Vector3.zero; //ジョイコン入力
+    private Vector3 _angle2 = Vector3.zero; //これは保存用
+    [SerializeField] private float Sensitivity = 0.1f; //感度
+    void Start()
+    {
+        //オイラー角は 0から360で表現するらしいので、負の角度は360から天引きとなる（例えば -30° は 330° となる）そのための計算。
+        MinDegreeX += 360;
+        MinDegreeY += 360;
+    }
+
+    void Update()
+    {
+        //射角制限に引っかかった時にロードするため、前フレームの角度を取得
+        _angle2 = this.gameObject.transform.localEulerAngles;
+
+        //Axis取得
+        _angle.x = Input.GetAxis("Right_Horizontal") * Sensitivity;
+        _angle.y = Input.GetAxis("Right_Vertical") * Sensitivity;
+
+        //クロスヘアの角度をジョイコンの入力で += する
+        this.gameObject.transform.localEulerAngles += _angle;
+
+        //射角制限
+        if (this.gameObject.transform.localEulerAngles.x <= MinDegreeX && this.gameObject.transform.localEulerAngles.x >= MinDegreeX - 10)
+        {
+            this.gameObject.transform.localEulerAngles = _angle2;
+        }
+        else if (this.gameObject.transform.localEulerAngles.x >= MaxDegreeX && this.gameObject.transform.localEulerAngles.x <= MaxDegreeX + 10)
+        {
+            this.gameObject.transform.localEulerAngles = _angle2;
+        }
+        else if (this.gameObject.transform.localEulerAngles.y <= MinDegreeY && this.gameObject.transform.localEulerAngles.y >= MinDegreeY - 10)
+        {
+            this.gameObject.transform.localEulerAngles = _angle2;
+        }
+        else if (this.gameObject.transform.localEulerAngles.y >= MaxDegreeY && this.gameObject.transform.localEulerAngles.y <= MaxDegreeY + 10)
+        {
+            this.gameObject.transform.localEulerAngles = _angle2;
+        }
+
+        //弾がレティクルの中心に飛ぶようにするにはカメラの位置と、Crosshairの座標が等しい必要がある。
+        //理由は弾道のベクトル計算にメインカメラの座標を用いているためである。
+        //VR化する際は、Updateの中で CameraRig座標 == Crosshair座標 とすればいいと思われる。
+        //もしくは、カメラの位置をいじるときはCameraControllerの位置をいじる（上の二つをposition(0,0,0)にする）。   ←今はこっち
+    }
+}
--- Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Manager/ShipController.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Manager/ShipController.cs
@@ -39,15 +39,96 @@ public class ShipController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Moving();
-        Rotating();
+        Moving_2nd();
+        //Moving();
+        //Rotating();
     }
+
+    void Moving_2nd()
+    {
+        //  入力値の取得
+        float moveX = Input.GetAxis("Left_Horizontal");
+        float moveY = Input.GetAxis("Left_Vertical");
+
+        //  入力がある場合
+        if (moveX != 0 || moveY != 0)
+        {
+            //  移動
+            rig.AddForce(moveX * movePower.x, moveY * movePower.y, 0);
+            if (Mathf.Abs(rig.velocity.x) > maxSpeed.x)
+            {
+                if (rig.velocity.x > 0)
+                    rig.velocity = new Vector3(Mathf.Min(rig.velocity.x, maxSpeed.x), rig.velocity.y, 0);
+                if (rig.velocity.x < 0)
+                    rig.velocity = new Vector3(Mathf.Max(rig.velocity.x, -maxSpeed.x), rig.velocity.y, 0);
+            }
+            if (Mathf.Abs(rig.velocity.y) > maxSpeed.y)
+            {
+                if (rig.velocity.y > 0)
+                    rig.velocity = new Vector3(rig.velocity.x, Mathf.Min(rig.velocity.y, maxSpeed.y), 0);
+                if (rig.velocity.y < 0)
+                    rig.velocity = new Vector3(rig.velocity.x, Mathf.Max(rig.velocity.y, -maxSpeed.y), 0);
+            }
+            //  回転 押しているかの判定だけ
+            if (moveX != 0)
+            {
+                freezeTimeX = 0;
+                rotateTimeX += Time.deltaTime;
+                rotateTimeX = Mathf.Min(rotateTimeX, 10.0f);
+            }
+            if (moveY != 0)
+            {
+                freezeTimeY = 0;
+                rotateTimeY += Time.deltaTime;
+                rotateTimeY = Mathf.Min(rotateTimeY, 3.0f);
+            }
+        }
+        //  入力がない場合、移動を止めるブレーキ処理
+        if (moveX == 0 && moveY == 0)
+        {
+            //  一定値未満で切り捨て 移動
+            if (rig.velocity.x < 0.1f && rig.velocity.x > -0.1f)
+                rig.velocity = new Vector2(0, rig.velocity.y);
+            if (rig.velocity.y < 0.1f && rig.velocity.y > -0.1f)
+                rig.velocity = new Vector2(rig.velocity.x, 0);
+            //  減速
+            rig.velocity = new Vector3(rig.velocity.x * brakePower.x, rig.velocity.y * brakePower.y);
+        }
+        //  回転減少
+        if (moveX == 0)
+        {
+            freezeTimeX += Time.deltaTime;
+            if (freezeTimeX > 0.15f)
+                rotateTimeX = 0f;
+            rotateTimeX -= Time.deltaTime;
+            rotateTimeX = Mathf.Max(0, rotateTimeX);
+        }
+        if (moveY == 0)
+        {
+            freezeTimeY += Time.deltaTime;
+            if (freezeTimeY > 0.15f)
+                rotateTimeY = 0f;
+            rotateTimeY -= Time.deltaTime;
+            rotateTimeY = Mathf.Max(0, rotateTimeY);
+        }
+
+        Debug.Log("X = " + rotateTimeX + " : " + "Y = " + rotateTimeY);
+        //  回転 割合
+        rig.rotation = Quaternion.Euler(moveY * -angle.y * Mathf.Lerp(0f,1f, rotateTimeY / 5.0f), 0, moveX * -angle.x * Mathf.Lerp(0f, 1f, rotateTimeX / 10.0f));
+
+    }
+
+    private float rotateTimeX = 0;
+    private float rotateTimeY = 0;
+    private float freezeTimeX = 0;
+    private float freezeTimeY = 0;
 
     void Moving()
     {
         //  移動処理
         float moveX = Input.GetAxisRaw("Left_Horizontal") * Time.deltaTime * movePower.x;
         float moveY = Input.GetAxisRaw("Left_Vertical") * Time.deltaTime * movePower.y;
+        //  移動の入力があったら
         if (moveX != 0 || moveY != 0)
         {
             rig.AddForce(moveX, moveY, 0);
@@ -87,18 +168,20 @@ public class ShipController : MonoBehaviour
             //  減速
             rig.velocity = new Vector3(rig.velocity.x * brakePower.x, rig.velocity.y * brakePower.y);
         }
-
     }
-
-    private float rotateTime = 0;
 
     private void Rotating()
     {
-        float rotateX = Input.GetAxis("Right_Horizontal");
-        float rotateY = Input.GetAxis("Right_Vertical");
+        float rotateX = Input.GetAxis("Left_Horizontal");
+        float rotateY = Input.GetAxis("Left_Vertical");
         //if (rotateX <= )
         Debug.Log("RotateX = " + rotateX + " : RotateY = " + rotateY);
         //rig.rotation = Quaternion.Euler(Mathf.Lerp(rotateY, 0, Time.deltaTime), 0, Mathf.Lerp(rotateX, 0, Time.deltaTime));
         rig.rotation = Quaternion.Euler(rotateY * -angle.y, 0, rotateX * -angle.x);
+    }
+
+    void Braking()
+    {
+
     }
 }
