47423f0df12f6de6b27469994bd00193dbdaf3f0
asahi <asahii.jr@gmail.com>
2019-11-25 16:34:23 +0900

    Enemyの基底クラスを作成

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//エネミーの基底クラス
+abstract public class Enemy : MonoBehaviour
+{
+    //Flag機かどうか
+    [SerializeField]
+    protected bool flagShip;
+    public bool FlagShip{
+        get { return flagShip; }
+    }
+    //エネミーのライフ
+    protected int enemyHp;
+    //エネミーの移動速度
+    protected float speed = 0.1f;
+    //エネミーの攻撃力
+    protected int attack;
+    //エネミーの弾の種類
+    protected GameObject bullet;
+
+    //Enemyの移動
+    virtual protected void Move() {
+        transform.Translate(0f, 0f,speed);
+    }
+
+    //Enemyのダメージ。インターフェースで実装
+
+    //Enemyの攻撃
+    virtual public void Attack() {
+
+    }
+
+}
