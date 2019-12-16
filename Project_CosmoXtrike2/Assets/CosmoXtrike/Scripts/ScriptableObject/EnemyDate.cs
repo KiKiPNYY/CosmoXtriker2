c7188411ad88eb2053531b3a4f6f7fe7bec09562
Suzuki <suzuki.nobu.527@gmail.com>
2019-12-16 15:34:03 +0900

    砲台の回転のやり方を変更

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/ScriptableObject/EnemyDate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "CreateScriptable/Create EnemyDate")]
+public class EnemyDate : ScriptableObject
+{
+    [SerializeField]
+    [Header("初期HP")]
+    int hitpoint;
+    [SerializeField]
+    [Header("移動速度")]
+    float speed = 1.0f;
+    [SerializeField]
+    [Header("攻撃力")]
+    int attack;
+
+    public int HP { get { return hitpoint; } }
+    public float Speed { get { return speed; } }
+    public int Attack { get { return attack; } }
+}
