3ef483977348b80a3c9738596d042e7ed84fdc76
高田将志 <masapo.0315@icloud.com>
2019-12-16 18:58:08 +0900

    カメラを親オブジェクトにくっつけてそれを回転させるようにした。

--- Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/CameraControl.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/CameraControl.cs
@@ -5,11 +5,19 @@ using Valve.VR;
 
 public class CameraControl : MonoBehaviour
 {
-    //上から回転させるカメラ、回転速度、ズーム距離、ズーム速度、アニメーションフラグ
+    //上から回転させるカメラ、回転速度
     [SerializeField] private GameObject cameraObject;
     [SerializeField] private float rotSpeed;
-    [SerializeField] private float zoomDistance;
+
+    //上からズームさせるカメラ、デフォルト値、ズーム最小値、ズーム速度
+    [SerializeField] private Camera camera;
+    [SerializeField] private float defView;
+    [SerializeField] private float minView;
     [SerializeField] private float zoomSpeed;
+
+    private float view;
+
+    //アニメーションフラグ
     private bool animFlag;
 
     //タイトルキャンバス
@@ -19,33 +27,37 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        view = camera.fieldOfView;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            animFlag = true;
+        view = camera.fieldOfView;
 
-        CameraRotation(rotSpeed);
-        CameraZoom(zoomSpeed);
+        if (Input.GetMouseButtonDown(0)) { animFlag = true; }
+
+        CameraRotation();
+        CameraZoom();
     }
 
-    private void CameraRotation(float Speed)
+    private void CameraRotation()
     {
         if (!animFlag) { return; }
 
-        iTween.RotateTo(cameraObject, iTween.Hash("y", -180f, "time", Speed));
+        iTween.RotateTo(cameraObject, iTween.Hash("y", -180f, "time", rotSpeed));
         Destroy(titleCanvas);
         //camera.transform.rotation = Quaternion.RotateTowards(camera.transform.rotation, Quaternion.Euler(0, 180f, 0), rotSpeed);
     }
 
-    private void CameraZoom(float Speed)
+    private void CameraZoom()
     {
         if (!animFlag) { return; }
 
+        iTween.ValueTo(camera.gameObject, iTween.Hash("from", view,"to", minView,"time", zoomSpeed));
+        //iTween.ValueTo(camera.gameObject, iTween.Hash("from", view,"to", defView,"time", zoomSpeed,"delay",zoomSpeed));
 
+        camera.fieldOfView = Mathf.Clamp(value: view, min: minView, max: defView);
 
         animFlag = false;
     }
