93b38cf2aea405da2589847bac4b22c104c6e7a0
高田将志 <masapo.0315@icloud.com>
2020-01-06 15:05:47 +0900

    タイトル画面にてカメラの回転、ズームイン、ズームアウトの実装

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/CameraContoroler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraContoroler : MonoBehaviour
+{
+     private float defalutView;                 //カメラの有効視野
+    [SerializeField] private float zoomInValue; //ズームインさせる値
+    [SerializeField] private float zoomSpeed;   //ズームイン＆アウトさせるのにかける時間
+    [SerializeField] private float roteSpeed;   //カメラを回転させる時間
+
+    //アニメーションフラグ
+    private bool animFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defalutView = Camera.main.fieldOfView;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボタンを押したらアニメーションフラグをオンにする
+        if (Input.GetMouseButtonDown(0)) { animFlag = true;}
+        CameraRote();
+        CameraZoomIn();
+        CameraZoomOut();
+    }
+
+    //カメラを回転
+    private void CameraRote()
+    {
+        if (!animFlag) { return; }
+        this.transform.DORotate(new Vector3(0, -180f, 0), roteSpeed);
+    }
+
+    //カメラをズームイン
+    private void CameraZoomIn()
+    {
+        if (!animFlag) { return; }
+        DOTween.To(() => Camera.main.fieldOfView, fovIn => Camera.main.fieldOfView = fovIn, zoomInValue, zoomSpeed);
+    }
+
+    //ズームインしている間遅延させ、再び元の倍率にズームアウト
+    private void CameraZoomOut()
+    {
+        if(!animFlag) { return; }
+        DOVirtual.DelayedCall(zoomSpeed,()=> { DOTween.To(() => Camera.main.fieldOfView, fovOut => Camera.main.fieldOfView = fovOut, defalutView, zoomSpeed + 1f); });
+        animFlag = false;
+    }
+}
