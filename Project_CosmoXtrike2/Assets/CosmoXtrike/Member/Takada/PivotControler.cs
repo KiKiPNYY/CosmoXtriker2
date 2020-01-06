46f6b5cefa0a19689ca3e6d09aef780b35fea947
高田将志 <masapo.0315@icloud.com>
2020-01-06 15:40:20 +0900

    コミット

---
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/PivotControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PivotControler : MonoBehaviour
+{
+    [SerializeField] private float roteTime;    //Pivotを回転させるのにかける時間
+    private bool roteflag;                      //回転させる際に使うフラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PivotRote();
+    }
+
+    //Pivotを回転させる
+    private void PivotRote()
+    {
+        //
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+    }
+
+}
