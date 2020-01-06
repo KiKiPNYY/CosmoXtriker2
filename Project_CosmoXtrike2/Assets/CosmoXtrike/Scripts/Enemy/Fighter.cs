1db67e1f0860811a43d07ede9888382ef26d7ba8
asahi <asahii.jr@gmail.com>
2020-01-06 18:36:02 +0900

    楕円を描いて周回移動する敵エネミーを作成

--- Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Enemy/Fighter.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Enemy/Fighter.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Fighter : Enemy{
 
 
+    bool turnMode = false;
 
 
     protected override void Move(){
         base.Move();
+        Turn(180, 3.0f);
+        if (!turnMode){ StartCoroutine( TurnStayCoroutine() );
+        }
 
     }
 
-
+    float timer = 0;
     private void Turn(float angle,float time) {
-        this.transform.Rotate(0, (angle/time)*Time.deltaTime , 0);
+        if (!turnMode) { Debug.Log(timer); return; }
+        timer += Time.deltaTime;
+        this.transform.Rotate(0, (angle / time) * Time.deltaTime, 0);
+        if ( timer >= time){
+            Debug.Log("turn End");
+            turnMode = false;
+            timer = 0;
+            return;
+        }
     }
 
-
+    bool isRunning = false;
+    IEnumerator TurnStayCoroutine() {
+        if (isRunning) yield break;
+        isRunning = true;
+        yield return new WaitForSeconds(3.0f);
+        turnMode = true;
+        isRunning = false;
+    }
+    
 }
