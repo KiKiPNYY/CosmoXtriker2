83b764b161d9d5ec0205d065cc592174dc431295
高田将志 <masapo.0315@icloud.com>
2020-01-17 16:40:28 +0900

    パネルのアニメーションを追加、選択画面で決定ボタンを押すとメインゲームへ行くようにした。

--- Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/Scripts/CameraContoroler.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/Scripts/CameraContoroler.cs
@@ -7,23 +7,21 @@ public class CameraContoroler : MonoBehaviour
 {
      private float defalutView;                 //カメラの有効視野
     [SerializeField] private float zoomInValue; //ズームインさせる値
-    [SerializeField] private float zoomSpeed;   //ズームイン＆アウトさせるのにかける時間
-    [SerializeField] private float roteSpeed;   //カメラを回転させる時間
+    [SerializeField] private float animSpeed;   //カメラを回転させる時間
 
     //アニメーションフラグ
     private bool animFlag = false;
-    public PanelAnimation panelAnimation;
     private Animator animator;
 
     //カメラのアニメーション後に消すタイトルオブジェクト
     [SerializeField] private GameObject titleObject;
 
+    [SerializeField] private PivotControler _pivotControler;
+
     // Start is called before the first frame update
     void Start()
     {
         defalutView = Camera.main.fieldOfView;
-        animator = panelAnimation.PanelAnim;
-
     }
 
     // Update is called once per frame
@@ -40,21 +38,21 @@ public class CameraContoroler : MonoBehaviour
     private void CameraRote()
     {
         if (!animFlag) { return; }
-        this.transform.DORotate(new Vector3(0, -180f, 0), roteSpeed);
+        this.transform.DORotate(new Vector3(0, -180f, 0), animSpeed);
     }
 
     //カメラをズームイン
     private void CameraZoomIn()
     {
         if (!animFlag) { return; }
-        DOTween.To(() => Camera.main.fieldOfView, fovIn => Camera.main.fieldOfView = fovIn, zoomInValue, zoomSpeed);
+        DOTween.To(() => Camera.main.fieldOfView, fovIn => Camera.main.fieldOfView = fovIn, zoomInValue, animSpeed / 2);
     }
 
     //ズームインしている間遅延させ、再び元の倍率にズームアウト
     private void CameraZoomOut()
     {
         if(!animFlag) { return; }
-        DOVirtual.DelayedCall(zoomSpeed,()=> { DOTween.To(() => Camera.main.fieldOfView, fovOut => Camera.main.fieldOfView = fovOut, defalutView, zoomSpeed + 1f); Destroy(titleObject); });
-        DOVirtual.DelayedCall(zoomSpeed + 2f,()=> { animator.SetBool("PanelOpen", true); animFlag = false; });
+        DOVirtual.DelayedCall(animSpeed / 2,()=> { DOTween.To(() => Camera.main.fieldOfView, fovOut => Camera.main.fieldOfView = fovOut, defalutView, animSpeed / 2); Destroy(titleObject); });
+        DOVirtual.DelayedCall(animSpeed / 2,()=> { _pivotControler.Panel1.SetActive(true); _pivotControler.PanelAnim.Play("PanelOpen"); _pivotControler.AnimFlag = true; animFlag = false; });
     }
 }
--- Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/Scripts/PivotControler.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Member/Takada/Scripts/PivotControler.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class PivotControler : MonoBehaviour
 {
     [SerializeField] private float roteTime;    //Pivotを回転させるのにかける時間
     [SerializeField] private float roteValue;   //回転させる値
-    private bool roteFlag;                      //回転させる際に使うフラグ
+    [HideInInspector] public bool AnimFlag;    
+    private bool roteFlag;
 
-    //パネルのアニメーション
+    //パネルとパネルのアニメーション
+    public GameObject Panel1;
     public Animator PanelAnim;
-    public Animator PanelAnim2;
+    [SerializeField] private GameObject Panel2;
+    [SerializeField] private Animator PanelAnim2;
+
+    //プレイヤーの番号
+    public static int playerNum;
 
     // Start is called before the first frame update
     void Start()
     {
+        //フラグなどの初期化
+        playerNum = 0;
+        AnimFlag = false;
         roteFlag = true;
     }
 
@@ -23,42 +33,47 @@ public class PivotControler : MonoBehaviour
     void Update()
     {
         PivotRote();
+        PlayerSelect();
     }
 
     //Pivotを回転させる
     private void PivotRote()
     {
+        if (!AnimFlag) { return; }
+
+        //Pivotの回転が終わったらパネルをアニメーション表示
         if (roteFlag && Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.gameObject.transform.DORotate(new Vector3(0f, roteValue * -1), roteTime);
+            Panel1.SetActive(false);
+            DOVirtual.DelayedCall(roteTime,()=> { Panel2.SetActive(true); PanelAnim2.Play("PanelOpen"); /*panel2Flag = false;*/});
 
             roteFlag = false;
         }
         else if (!roteFlag && Input.GetKeyDown(KeyCode.RightArrow))
         {
             this.gameObject.transform.DORotate(new Vector3(0f, roteValue - roteValue), roteTime);
+            Panel2.SetActive(false);
+            DOVirtual.DelayedCall(roteTime,()=>{ Panel1.SetActive(true); PanelAnim.Play("PanelOpen"); /*panelFlag = false;*/ });
 
             roteFlag = true;
         }
     }
 
-    private IEnumerator Panel1Anim()
+    //自機選択
+    private void PlayerSelect()
     {
-        PanelAnim2.SetBool("ClosePanel", false);
-
-        yield return new WaitForSeconds(roteTime);
-
-        PanelAnim.SetBool("OpenPanel", true);
-
-    }
-
-    private IEnumerator Panel2Anim()
-    {
-        PanelAnim.SetBool("ClosePanel", true);
-
-        yield return new WaitForSeconds(roteTime);
-
-        PanelAnim2.SetBool("OpenPanel", false);
+        //ボタンを押したらメインゲームへ
+        if (roteFlag && Input.GetKeyDown(KeyCode.Return))
+        {
+            playerNum = 1;
+            SceneManager.LoadScene("Game");
+        }
+        else if(!roteFlag && Input.GetKeyDown(KeyCode.Return))
+        {
+            playerNum = 2;
+            SceneManager.LoadScene("Game");
+        }
     }
 
 }
