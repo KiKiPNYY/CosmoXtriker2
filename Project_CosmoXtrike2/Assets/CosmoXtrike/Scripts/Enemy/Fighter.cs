fd8b792ceb8f96d1ab9d43f0461ca69d146f4659
asahi1998 <asahii.jr@gmail.com>
2020-02-10 00:51:24 +0900

    プレハブとFighterをちょい修正

--- Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Enemy/Fighter.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/Enemy/Fighter.cs
@@ -16,6 +16,9 @@ public class Fighter : Enemy{
     [SerializeField]
     bool target = false;
 
+    [SerializeField]
+    float soriteTime = 5.0f;
+
     //playerのtransform
     Transform lockOnTransform;
 
@@ -238,7 +241,7 @@ public class Fighter : Enemy{
 
     IEnumerator SoriteCoroutine(){
         sorite = true;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(soriteTime);
         sorite = false;
         goFirstPoint = true;
     }
