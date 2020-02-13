d15cf9dcbe09c6723fdb26a709afbc1e62069828
Asahi <asahii.jr@gmail.com>
2020-02-13 19:31:06 +0900

    parameterの必要ない項目を非表示

--- Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/ScriptableObject/EnemyDate.cs
+++ Project_CosmoXtrike2/Assets/CosmoXtrike/Scripts/ScriptableObject/EnemyDate.cs
@@ -10,11 +10,11 @@ public class EnemyDate : ScriptableObject
     [SerializeField]
     [Header("移動速度")]
     float speed = 1.0f;
-    [SerializeField]
-    [Header("攻撃力")]
-    int attack;
+    //[SerializeField]
+    //[Header("攻撃力")]
+    //int attack;
 
     public int HP { get { return hitpoint; } }
     public float Speed { get { return speed; } }
-    public int Attack { get { return attack; } }
+    //public int Attack { get { return attack; } }
 }
